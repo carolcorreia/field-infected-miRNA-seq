f578244f4ab384dbc31fe834be3ed141d5b5e740
Carolina Correia <carolina.correia@ucdconnect.ie>
2020-07-23 15:33:57 +0100

    cutadapt, fastqc trimmed

--- Linux-bash/01-Field-infected-miRNA-seq_QC_filter.sh
+++ Linux-bash/01-Field-infected-miRNA-seq_QC_filter.sh
@@ -4,7 +4,7 @@
 ################################################################
 
 # Author: Carolina N. Correia 
-# Last updated on: 21/07/2020
+# Last updated on: 23/07/2020
 
 ################################
 # Download and files check sum #
@@ -48,22 +48,17 @@ done
 # FastQC quality check of raw FASTQ files #
 ###########################################
 
-# Required software is FastQC v0.11.5, consult manual/tutorial
+# Required software is FastQC v0.11.8, consult manual/tutorial
 # for details: http://www.bioinformatics.babraham.ac.uk/projects/fastqc/
 
 # Create and enter the quality check output directory:
-mkdir -p $HOME/scratch/miRNAseqValidation/quality_check/pre-filtering
+mkdir -p /home/workspace/ccorreia/miRNASeq_field/quality_check/pre-filtering
 cd !$
 
-# Run FastQC in one file to see if it's working well:
-fastqc -o $HOME/scratch/miRNAseqValidation/quality_check/pre-filtering \
---noextract --nogroup -t 2 \
-$HOME/storage/miRNAseqValidation/20160429_miRNASeq/E10_CGTACG_L005_R1_001.fastq.gz
-
 # Create a bash script to perform FastQC quality check on all fastq.gz files:
-for file in `find $HOME/storage/miRNAseqValidation/20160429_miRNASeq \
--name *fastq.gz`; do echo "fastqc --noextract --nogroup -t 2 \
--o $HOME/scratch/miRNAseqValidation/quality_check/pre-filtering $file" \
+for file in `find /home/workspace/ccorreia/miRNASeq_field/fastq \
+-name *fastq.gz`; do echo "fastqc --noextract --nogroup -t 10 \
+-o /home/workspace/ccorreia/miRNASeq_field/quality_check/pre-filtering $file" \
 >> fastqc.sh; done
 
 # Split and run all scripts on Stampede:
@@ -74,49 +69,78 @@ chmod 755 $script
 nohup ./$script > ${script}.nohup &
 done
 
+# Deleted all the HTML files:
+rm -r *.html
+
 # Check if all the files were processed:
 for file in `ls fastqc.sh.0*.nohup`; \
 do more $file | grep "Failed to process file" >> failed_fastqc.txt
 done
 
-# Deleted all the HTML files:
-rm -r *.html
-
 # Check all output from FastQC:
-mkdir $HOME/scratch/miRNAseqValidation/quality_check/pre-filtering/tmp
+mkdir /home/workspace/ccorreia/miRNASeq_field/quality_check/pre-filtering/tmp
 
 for file in `ls *_fastqc.zip`; do unzip \
-$file -d $HOME/scratch/miRNAseqValidation/quality_check/pre-filtering/tmp; \
+$file -d /home/workspace/ccorreia/miRNASeq_field/quality_check/pre-filtering/tmp; \
 done
 
 for file in \
-`find $HOME/scratch/miRNAseqValidation/quality_check/pre-filtering/tmp \
--name summary.txt`; do more $file >> reports_pre-filtering.txt; \
+`find /home/workspace/ccorreia/miRNASeq_field/quality_check/pre-filtering/tmp \
+-name summary.txt`; do more $file >> summary_pre-filtering.txt; \
 done
 
 for file in \
-`find $HOME/scratch/miRNAseqValidation/quality_check/pre-filtering/tmp \
+`find /home/workspace/ccorreia/miRNASeq_field/quality_check/pre-filtering/tmp \
 -name fastqc_data.txt`; do head -n 10 $file >> basic_stats_pre-filtering.txt; \
 done
 
+# Check sequence quality:
+grep 'Per base sequence quality' summary_pre-filtering.txt >> seq_quality.txt
+wc -l seq_quality.txt # 48 lines
+grep PASS seq_quality.txt | wc -l # 48 lines
+grep WARN seq_quality.txt | wc -l # 0 line
+
+# Check if sequence contain adapters:
+grep 'Adapter Content' summary_pre-filtering.txt >> adapter_content.txt
+wc -l adapter_content.txt # 48 lines
+grep PASS adapter_content.txt | wc -l # 0 lines
+grep WARN adapter_content.txt | wc -l # 0 lines
+grep FAIL adapter_content.txt | wc -l # 48 lines
+
+# Check sequence length:
+grep 'Sequence length' basic_stats_pre-filtering.txt >> seq_length.txt
+wc -l seq_length.txt # 48 lines
+less seq_length.txt # 50 for all
+
+# Check per base sequence quality
+grep 'Per base sequence quality' summary_pre-filtering.txt >> base_quality.txt
+wc -l base_quality.txt # 48 lines
+grep PASS base_quality.txt | wc -l # 48 lines
+grep WARN base_quality.txt | wc -l # 0 lines
+
+# Transfer compressed folders to personal laptop via SCP
+# and check HTML reports:
+scp -r \
+ccorreia@rodeo.ucd.ie:/home/workspace/ccorreia/miRNASeq_field/quality_check/pre-filtering/tmp .
+
 # Remove temporary folder and its files:
-rm -rf $HOME/scratch/miRNAseqValidation/quality_check/pre-filtering/tmp
+rm -r tmp
 
 #############################################
 # Trimming of adapter sequence within reads #
 #############################################
 
-# Required software is cutadapt (version 1.10).
+# Required software is cutadapt (version 2.9).
 # Consult manual for details:
 # https://cutadapt.readthedocs.io/en/stable/guide.html
 
 # Create and enter working directory:
-mkdir $HOME/scratch/miRNAseqValidation/fastq_sequence/
+mkdir /home/workspace/ccorreia/miRNASeq_field/fastq_trimmed
 cd !$
 
 # Create bash script to trim the Illumina RNA 3’ Adapter (RA3) of each
 # FASTQ file while keeping the sequencing lane information:
-for file in `find $HOME/storage/miRNAseqValidation/20160429_miRNASeq \
+for file in `find /home/workspace/ccorreia/miRNASeq_field/fastq \
 -name *fastq.gz`; \
 do outfile=`basename $file | perl -p -e 's/.fastq.gz//'`; \
 echo "cutadapt -a TGGAATTCTCGGGTGCCAAGG -O 10 --match-read-wildcards \
@@ -137,29 +161,29 @@ grep -c 'Finished' cutadapt.sh.00.nohup
 grep -c 'Finished' cutadapt.sh.01.nohup
 
 # Generate a master file containing cutadapt trimming stats results:
-for file in `ls $HOME/scratch/miRNAseqValidation/fastq_sequence/*.nohup`; \
+for file in `ls /home/workspace/ccorreia/miRNASeq_field/fastq_trimmed/*.nohup`; \
 do grep -oP "E\d+_\w+_L00(5|6)\_R1_001.fastq.gz" $file \
->> $HOME/scratch/miRNAseqValidation/fastq_sequence/filename.txt; \
+>> /home/workspace/ccorreia/miRNASeq_field/fastq_trimmed/filename.txt; \
 grep "Total reads processed\:" $file | \
 perl -p -e 's/\w*\s\w*\s\w*\:\s*(\d*.\d*.\d*)/$1/' \
->> $HOME/scratch/miRNAseqValidation/fastq_sequence/processed.txt; \
+>> /home/workspace/ccorreia/miRNASeq_field/fastq_trimmed/processed.txt; \
 grep "Reads with adapters\:" $file | \
 perl -p -e 's/\w*\s\w*\s\w*\:\s*(\d*.\d*.\d*)\s.*/$1/' \
->> $HOME/scratch/miRNAseqValidation/fastq_sequence/reads_with_adapters.txt; \
+>> /home/workspace/ccorreia/miRNASeq_field/fastq_trimmed/reads_with_adapters.txt; \
 grep "Reads that were too short\:" $file | \
 perl -p -e 's/\w*\s\w*\s\w*\s\w*\s\w*\:\s*(\d*.\d*.\d*)\s.*/$1/' \
->> $HOME/scratch/miRNAseqValidation/fastq_sequence/short.txt; \
+>> /home/workspace/ccorreia/miRNASeq_field/fastq_trimmed/short.txt; \
 grep "Reads written (passing filters)\:" $file | \
 perl -p -e 's/\w*\s\w*\s.\w*\s\w*.\:\s*(\d*.\d*.\d*)\s.*/$1/' \
->> $HOME/scratch/miRNAseqValidation/fastq_sequence/trimmed.txt; \
+>> /home/workspace/ccorreia/miRNASeq_field/fastq_trimmed/trimmed.txt; \
 paste filename.txt processed.txt reads_with_adapters.txt short.txt trimmed.txt \
-> $HOME/scratch/miRNAseqValidation/fastq_sequence/trimmed_stats.txt; \
+> /home/workspace/ccorreia/miRNASeq_field/fastq_trimmed/trimmed_stats.txt; \
 done
 
 echo -e "Sample\tTotal reads processed\tReads with \
 adapters\tReads that were too short\tReads written (passing filters)\t" \
-| cat - $HOME/scratch/miRNAseqValidation/fastq_sequence/trimmed_stats.txt \
-> $HOME/scratch/miRNAseqValidation/fastq_sequence/trimming_stats.txt
+| cat - /home/workspace/ccorreia/miRNASeq_field/fastq_trimmed/trimmed_stats.txt \
+> /home/workspace/ccorreia/miRNASeq_field/fastq_trimmed/trimming_stats.txt
 
 rm -r filename.txt processed.txt reads_with_adapters.txt \
 short.txt trimmed.txt trimmed_stats.txt
@@ -168,22 +192,17 @@ short.txt trimmed.txt trimmed_stats.txt
 # FastQC quality check of trimmed FASTQ files #
 ###############################################
 
-# Required software is FastQC v0.11.5, consult manual/tutorial
+# Required software is FastQC v0.11.8, consult manual/tutorial
 # for details: http://www.bioinformatics.babraham.ac.uk/projects/fastqc/
 
 # Create and enter working directory:
-mkdir $HOME/scratch/miRNAseqValidation/quality_check/post_filtering
+mkdir /home/workspace/ccorreia/miRNASeq_field/quality_check/post_filtering
 cd !$
 
-# Run FastQC in one file to see if it's working well:
-fastqc -o $HOME/scratch/miRNAseqValidation/quality_check/post_filtering \
---noextract --nogroup -t 2 \
-$HOME/scratch/miRNAseqValidation/fastq_sequence/E10_CGTACG_L005_R1_001_trim.fastq.gz
-
 # Create bash script to perform FastQC quality check on all trim.fastq.gz files:
-for file in `find $HOME/scratch/miRNAseqValidation/fastq_sequence \
--name *trim.fastq.gz`; do echo "fastqc --noextract --nogroup -t 2 \
--o $HOME/scratch/miRNAseqValidation/quality_check/post_filtering $file" \
+for file in `find /home/workspace/ccorreia/miRNASeq_field/fastq_trimmed \
+-name *trim.fastq.gz`; do echo "fastqc --noextract --nogroup -t 10 \
+-o /home/workspace/ccorreia/miRNASeq_field/quality_check/post_filtering $file" \
 >> fastqc.sh; done
 
 # Split and run all scripts on Stampede:
@@ -194,30 +213,59 @@ chmod 755 $script
 nohup ./$script > ${script}.nohup &
 done
 
+# Deleted all the HTML files:
+rm -r *.html
+
 # Check if all the files were processed:
 for file in `ls fastqc.sh.0*.nohup`; \
 do more $file | grep "Failed to process file" >> failed_fastqc.txt
 done
 
-# Deleted all the HTML files:
-rm -r *.html
-
 # Check all output from FastQC:
-mkdir tmp
+mkdir /home/workspace/ccorreia/miRNASeq_field/quality_check/post_filtering/tmp
 
 for file in `ls *_fastqc.zip`; do unzip \
-$file -d $HOME/scratch/miRNAseqValidation/quality_check/post_filtering/tmp; \
+$file -d /home/workspace/ccorreia/miRNASeq_field/quality_check/post_filtering/tmp; \
 done
 
 for file in \
-`find $HOME/scratch/miRNAseqValidation/quality_check/post_filtering/tmp \
--name summary.txt`; do more $file >> reports_post-filtering.txt; \
+`find /home/workspace/ccorreia/miRNASeq_field/quality_check/post_filtering/tmp \
+-name summary.txt`; do more $file >> summary_post-filtering.txt; \
 done
 
 for file in \
-`find $HOME/scratch/miRNAseqValidation/quality_check/post_filtering/tmp \
+`find /home/workspace/ccorreia/miRNASeq_field/quality_check/post_filtering/tmp \
 -name fastqc_data.txt`; do head -n 10 $file >> basic_stats_post-filtering.txt; \
 done
+
+# Check sequence quality:
+grep 'Per base sequence quality' summary_post-filtering.txt >> seq_quality.txt
+wc -l seq_quality.txt # 48 lines
+grep PASS seq_quality.txt | wc -l # 48 lines
+grep WARN seq_quality.txt | wc -l # 0 line
+
+# Check if sequence contain adapters:
+grep 'Adapter Content' summary_post-filtering.txt >> adapter_content.txt
+wc -l adapter_content.txt # 48 lines
+grep PASS adapter_content.txt | wc -l # 48 lines
+grep WARN adapter_content.txt | wc -l # 0 lines
+grep FAIL adapter_content.txt | wc -l # 0 lines
+
+# Check sequence length:
+grep 'Sequence length' basic_stats_post-filtering.txt >> seq_length.txt
+wc -l seq_length.txt # 48 lines
+less seq_length.txt # 17-41 for all
+
+# Check per base sequence quality
+grep 'Per base sequence quality' summary_post-filtering.txt >> base_quality.txt
+wc -l base_quality.txt # 48 lines
+grep PASS base_quality.txt | wc -l # 48 lines
+grep WARN base_quality.txt | wc -l # 0 lines
+
+# Transfer compressed folders to personal laptop via SCP
+# and check HTML reports:
+scp -r \
+ccorreia@rodeo.ucd.ie:/home/workspace/ccorreia/miRNASeq_field/quality_check/post_filtering/tmp .
 
 # Remove temporary folder and its files:
 rm -r tmp
